b1ae508c68e9a948722f909e34ead7343f60584e
Kushagra9838 <119300198+Kushagra9838@users.noreply.github.com>
2025-04-19 00:21:21 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,29 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { LoggerModule } from './logger/logger.module';  // Correct import
-import { AuthModule } from './auth/auth.module'; 
+import { LoggerModule } from './logger/logger.module';
+import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ContactsModule } from './contacts/contacts.module';
 import { MessageModule } from './message/message.module';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
-    LoggerModule,  
-    AuthModule,
-    MongooseModule.forRoot("mongodb://localhost:27017/chatApplication"),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    LoggerModule,
+    AuthModule,
     ContactsModule,
     MessageModule,
     NotificationModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
